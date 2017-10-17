4efadbff40d77bce1cf463c1b8260d2f44a064d2
VicSvensson <Victor.svensson3@gmail.com>
2017-10-17 15:01:31 +0200

    RSS Lathund"vad behöver vi göra idag?"

---
+++ WFA Podcast/Datalayer/RSS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalayer
+{
+    class RSS
+    {
+        var xml = "Namn på XML fil";
+      using (var client = new System.Net.WebClient())
+      {
+         client.Encoding = Encoding.UTF8;
+         xml = client.DownloadString("RSS URL");
+      }
+
+//Skapa en objektrepresentation.
+var dom = new System.Xml.XmlDocument();
+dom.LoadXml(xml);
+
+      //Iterera igenom elementet item.
+      foreach (System.Xml.XmlNode item
+         in dom.DocumentElement.SelectNodes("Items i en List"))
+      {
+         //Skriv ut dess titel.
+         var title = item.SelectSingleNode("Vad detta ska heta");
+Console.WriteLine(title.InnerText);
+      }
+
+    }
+}
