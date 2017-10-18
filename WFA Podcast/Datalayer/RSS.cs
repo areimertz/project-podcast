f057372bf51f8771bea486ca70c0d8ac18994e74
Jorgensendavid <david.jorgensen95@outlook.com>
2017-10-18 15:43:47 +0200

    rss internal

--- WFA Podcast/Datalayer/RSS.cs
+++ WFA Podcast/Datalayer/RSS.cs
@@ -9,13 +9,18 @@ namespace Datalayer
 {
     public class RSS
     {
+        internal class InternalClass
+        {
+            internal string xml = "";
+        }
 
         public class Rss : XmlAbstract
         {
             public override XmlDocument PodDocument(string urlPod)
             {
+                InternalClass iClass = new InternalClass();
+                var xml = iClass.xml;
 
-                var xml = "";
                 using (var client = new System.Net.WebClient())
                 {
                     client.Encoding = Encoding.UTF8;
